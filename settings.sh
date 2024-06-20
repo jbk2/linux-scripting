39ace1f58c32a025b29aa14b8d82fefa47c730af
jbk2 <james@humanbenefits.com>
2024-06-20 13:19:32 +0200

    Enable cmmd line arguments when executing setup.sh
    - Modularise the setup.sh script into units and steps
    - catch -u -s command line flags with arguments and conditionally check
      arguments agains unit and step names to determine whether to run script.
    - Add ANSI color to output for easier reading.

--- settings.sh
+++ settings.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-USER=deploy
-PORT=22 # default ssh port
-SSH_OPTIONS="-i $SSH_KEY -o StrictHostKeyChecking=no"
+USER=${USER:-deploy}
+SERVER=${SERVER:-13.36.100.115}
+SERVER_NAME=${SERVER_NAME:-www.ctrlaltdelete.com}
+PORT=${PORT:-22} # default ssh port
+SSH_KEY=${SSH_KEY:-~/Documents/Code/Credentials/toy-app1-keypair.pem}
+SSH_OPTIONS=${SSH_OPTIONS:-"-i $SSH_KEY -o StrictHostKeyChecking=no"}
 
 UBUNTU_SSH_ARGS="ubuntu@$SERVER -p $PORT $SSH_OPTIONS"
 APP_SSH_ARGS="$USER@$SERVER -p $PORT $SSH_OPTIONS"
@@ -17,6 +20,6 @@ ssh_as_user() {
 }
 
 fail() {
-  echo "Failed: $1"
+  echo -e "Failed: $1"
   exit 1
 }
