9a9dec739a033bf9993d35f1d39aec3462f1e396
jbk2 <james@humanbenefits.com>
2024-07-18 12:18:51 +0200

    Commenting, index update & move all vars into settings.sh

--- settings.sh
+++ settings.sh
@@ -12,15 +12,27 @@ NC="\033[0m"          # No color
 
 # ----------------------------------------
 
+# Provisioning, admin & SSH related variables
 USER=${USER:-deploy}
 SERVER=${SERVER:-13.39.86.143}
 SERVER_NAME=${SERVER_NAME:-www.ctrlaltdelete.com}
-PORT=${PORT:-22} # default ssh port
+PORT=${PORT:-22} # the default ssh port
 SSH_KEY=${SSH_KEY:-~/Documents/Code/Credentials/toy-app1-keypair.pem}
 SSH_OPTIONS=${SSH_OPTIONS:-"-i $SSH_KEY -o StrictHostKeyChecking=no"}
-
 UBUNTU_SSH_ARGS="ubuntu@$SERVER -p $PORT $SSH_OPTIONS"
 APP_SSH_ARGS="$USER@$SERVER -p $PORT $SSH_OPTIONS"
+
+# ----------------------------------------
+
+# DNS related variables
+CF_API_TOKEN="M4EoSJUf8erouwLe0SV6mNhLdG1J0skfsHFw4I9F"
+ZONE_ID="8914d5652221e66de395d0d3b2a63210"
+RECORD_ID1="6dc3fdb2420bd2c36d2e44eb8c2e0999"  # Record ID for ctrlaltinsure.com
+RECORD_NAME1="ctrlaltinsure.com"
+RECORD_ID2="00cc89e395517ddd7b6ace5d65b89876"  # Record ID for www.ctrlaltinsure.com
+RECORD_NAME2="www.ctrlaltinsure.com"
+
+# ----------------------------------------
 
 ssh_as_ubuntu() {
   ssh $UBUNTU_SSH_ARGS $@
